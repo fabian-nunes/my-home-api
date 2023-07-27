cc33260a9f7b0b1c9938010c607b4bdd8bb76bba
fabian-nunes <fabiannunes12@gmail.com>
2023-07-27 08:24:30 +0100

    Send data to frontend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 from settings import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 
 app = Flask(__name__)
+cors = CORS(app)
 
 # MySQL configurations
 app.config['MYSQL_USER'] = MYSQL_USER
