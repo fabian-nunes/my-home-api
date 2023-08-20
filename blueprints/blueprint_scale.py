dce18a3d29328d4a98224fd74d477f676c5a3d03
fabian-nunes <fabiannunes12@gmail.com>
2023-08-20 11:52:31 +0100

    fix scale update

--- blueprints/blueprint_scale.py
+++ blueprints/blueprint_scale.py
@@ -10,19 +10,20 @@ scale = Blueprint('scale', __name__)
 @jwt_required()
 def data():
     if request.method == 'GET':
-        current_scale = db_read("SELECT weight as value, createdAt, alert from scale order by createdAt DESC limit 1")
-        if len(current_scale) == 1:
-            return jsonify(current_scale[0])
-        return Response(status=409, response="No data stored")
-    elif request.method == 'POST':
-        image = request.files['img']
-        user = request.form['user']
         current_user = get_jwt_identity()
         if current_user:
-            if user != current_user:
-                return Response(status=401, response="Unauthorized")
+            current_scale = db_read(
+                "SELECT weight as value, createdAt, alert from scale order by createdAt DESC limit 1")
+            if len(current_scale) == 1:
+                return jsonify(current_scale[0])
+            return Response(status=409, response="No data stored")
+        return Response(status=401, response="Unauthorized")
+    elif request.method == 'POST':
+        image = request.files['img']
+        current_user = get_jwt_identity()
+        if current_user:
 
-            #get id from user
+            # get id from user
             user_id = db_read("SELECT id FROM user WHERE email = %s", (current_user,))
             values = process_image(image, user_id)
             if values:
